fa4871dabb3e5578afcfc42cdbf81d5e2f67a908
Nathanael <natsuxgraph@gmail.com>
2020-03-16 16:07:22 +0100

    08 Variable for title

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.set('view engine', 'pug');
 
 
 app.get('/', function(req, res) {
-  res.render('index');
+  res.render('index', { title : 'Qui prend quoi ?'});
 });
 
 app.listen(process.env.PORT, () => console.log(`Front app listening on port ${process.env.PORT}!`));
