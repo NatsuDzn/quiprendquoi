3747d70157ba18037edb090d7d121ec399120a8a
Nathanael <natsuxgraph@gmail.com>
2020-03-16 15:55:18 +0100

    04 Auto refresh with nodemon

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', function(req, res) {
-  res.send('Welcome to quiprendquoi');
+  res.send('Welcome to the Application');
 });
 
 app.listen(port, () => console.log(`Front app listening on port ${port}!`));
