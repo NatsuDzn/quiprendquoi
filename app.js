7df29498881f2102e292aeb0f5d241ba5d65bcd5
Nathanael <natsuxgraph@gmail.com>
2020-03-16 16:01:23 +0100

    06 Add pug

--- app.js
+++ app.js
@@ -4,9 +4,11 @@ const app = express();
 // Environement 
 const dotenv = require('dotenv').config();
 
+app.set('view engine', 'pug');
+
 
 app.get('/', function(req, res) {
-  res.send('Welcome to the Application');
+  res.render('index');
 });
 
 app.listen(process.env.PORT, () => console.log(`Front app listening on port ${process.env.PORT}!`));
